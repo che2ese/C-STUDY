4ae205f6eeadacc4e155f268e8d8dadc45e13ba7
MS_1129 <141838530+che2ese@users.noreply.github.com>
2024-03-13 18:25:05 +0900

    Add files via upload

---
+++ C#/기초실습/간단한 자동 클릭 매크로/MouseClick/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace MouseClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(92, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 1);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(92, 143);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 1);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "종료";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(800, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(86, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(86, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(166, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "\r\n\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(166, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "\r\n\r\n";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ C#/기초실습/간단한 자동 클릭 매크로/MouseClick/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClick
+{
+    public partial class Form1 : Form
+    {
+        int Tick = 0;
+        // 위도우 USER 구성 요소 ( 창, 메뉴, 윈도우 사용자 인터페이스 ) 등의 구현을 지원
+        [DllImport("user32.dll")] 
+
+        // mouse_event(이벤트 종류, x좌표, y좌표, 휠 스크롤, 마우스 타입)
+        static extern void mouse_event(uint dwFlags, uint dx , uint dy, uint dwData, int dwExtraInfo);
+        // const : 상수
+        const uint L_BUTTON_DOWN = 0x0002; // 지정한 값에 따라 이벤트 변경
+        const uint L_BUTTON_UP = 0x0004;
+        const uint R_BUTTON_DOWN = 0x0008;
+        const uint R_BUTTON_UP = 0x0010;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = Cursor.Position.X.ToString();
+            label5.Text = Cursor.Position.Y.ToString();
+            Tick++;
+            if(Tick == 30)
+            {
+                Cursor.Position = new Point(500, 391);
+
+                mouse_event(R_BUTTON_DOWN, 0, 0, 0, 0);
+                mouse_event(R_BUTTON_UP, 0, 0, 0, 0);
+            }
+            if (Tick == 60)
+            {
+                Cursor.Position = new Point(800, 391);
+
+                mouse_event(R_BUTTON_DOWN, 0, 0, 0, 0);
+                mouse_event(R_BUTTON_UP, 0, 0, 0, 0);
+                Tick = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
