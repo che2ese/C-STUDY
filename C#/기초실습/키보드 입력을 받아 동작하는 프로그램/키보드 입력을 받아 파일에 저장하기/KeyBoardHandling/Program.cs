dc873527725426cd6f4d284ec2793d1b31ae8349
MS_1129 <141838530+che2ese@users.noreply.github.com>
2024-03-13 17:08:04 +0900

    Add files via upload

---
+++ C#/기초실습/키보드 입력을 받아 동작하는 프로그램/키보드 입력을 받아 파일에 저장하기/KeyBoardHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyBoardHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string KeyData = "";
+            Console.WriteLine("파일에 저장할 내용 입력 : ");
+            KeyData = Console.ReadLine();
+            Console.WriteLine("입력한 값 : {0}", KeyData);
+            StreamWriter Mw1 = new StreamWriter(new FileStream("KeyBoardHandling.txt", FileMode.Create));
+            Mw1.WriteLine(KeyData);
+            Mw1.Close();
+
+            StreamReader Mr1 = new StreamReader(new FileStream("KeyBoardHandling.txt", FileMode.Open));
+            while (Mr1.EndOfStream == false)
+            {
+                Console.WriteLine(Mr1.ReadLine());
+            }
+            Mr1.Close();
+            Console.ReadLine();
+        }
+    }
+}
